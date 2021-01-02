6c242e2b1cc13739121aebd65326cd89ed2e0bad
zsagia <zsagia@gmail.com>
2021-01-02 17:04:11 +0100

    Add ng-zorro with base content

--- projects/ape-web/src/app/app.module.ts
+++ projects/ape-web/src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_GB } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+registerLocaleData(en);
 
 @NgModule({
     declarations: [AppComponent],
@@ -19,8 +31,14 @@ import { AppComponent } from './app.component';
             logOnly: environment.production,
         }),
         EffectsModule.forRoot([]),
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        IconsProviderModule,
+        NzLayoutModule,
+        NzMenuModule,
     ],
-    providers: [],
+    providers: [{ provide: NZ_I18N, useValue: en_GB }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
