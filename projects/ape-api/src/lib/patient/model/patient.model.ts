c0a601d0d75527e23db892ee060e85a27e2effd8
zsagia <zsagia@gmail.com>
2021-01-04 11:27:14 +0100

    Add patinet data to data.service

--- projects/ape-api/src/lib/patient/model/patient.model.ts
+++ projects/ape-api/src/lib/patient/model/patient.model.ts
@@ -1,16 +1,23 @@
 import { EntityModel } from '../../entity';
 
 export interface PatientModel extends EntityModel {
-    admitDate: number;
+    abxScore: number;
+    address: string;
+    admitDate: string;
+    age: number;
+    aspNotifications: string;
     bed: string;
-    birthdate: number;
-    firstName: string;
-    gender: string;
-    lastName: string;
-    mrn: number;
-    room: string;
+    currentAntibiotics: string;
+    daysOnAntibiotics: number;
+    email: string;
+    guid: string;
+    index: number;
+    name: any;
+    nextReview: string;
+    phone: string;
+    room: number;
     unit: string;
     userIds: string[];
     weight: number;
-    workingDiagnoses: string[];
+    workingDiagnose: string;
 }
